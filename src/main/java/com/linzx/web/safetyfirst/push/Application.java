9facf4d33781ab6ac4b5ba53c0e5efb28bff7ea2
Lin <279050223@qq.com>
2018-12-21 20:54:54 +0800

    application

--- src/main/java/com/linzx/web/safetyfirst/push/Application.java
+++ src/main/java/com/linzx/web/safetyfirst/push/Application.java
@@ -1,5 +1,21 @@
 package com.linzx.web.safetyfirst.push;
 
-public class Application {
+import java.util.logging.Logger;
 
+import org.glassfish.jersey.server.ResourceConfig;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.linzx.web.safetyfirst.push.service.AccountService;
+
+public class Application extends ResourceConfig{
+	public Application() {
+		//注册逻辑处理的包名
+		packages(AccountService.class.getPackage().getName());
+		
+		//注册json解析器
+		register(JacksonJsonProvider.class);
+		
+		//注册日志打印输出
+		register(Logger.class);
+	}
 }
