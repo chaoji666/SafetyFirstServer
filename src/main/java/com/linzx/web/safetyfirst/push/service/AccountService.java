a7d96a74b92357662ba7bbe1bd7f2139cf6de375
Lin <279050223@qq.com>
2018-12-21 22:12:27 +0800

    .

--- src/main/java/com/linzx/web/safetyfirst/push/service/AccountService.java
+++ src/main/java/com/linzx/web/safetyfirst/push/service/AccountService.java
@@ -1,7 +1,13 @@
 package com.linzx.web.safetyfirst.push.service;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.linzx.web.safetyfirst.push.bean.User;
 
 //127.0.0.1/api/account/...
 @Path("/account")
@@ -12,5 +18,17 @@ public class AccountService {
 	@Path("/login")
 	public String get() {
 		return "you get the login.";
+	}
+	
+	@POST
+	@Path("/login")
+	//指定请求与返回的响应体为json
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User post() {
+		User user = new User();
+		user.setName("妹纸");
+		user.setSex(2);
+		return user;
 	}
 }
