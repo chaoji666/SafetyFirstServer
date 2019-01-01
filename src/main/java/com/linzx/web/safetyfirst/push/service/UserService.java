45703baf4749b19ef45a256f028c577a43c7f1e9
Lin <279050223@qq.com>
2019-01-01 22:48:02 +0800

    关注人、搜索人接口实现

--- src/main/java/com/linzx/web/safetyfirst/push/service/UserService.java
+++ src/main/java/com/linzx/web/safetyfirst/push/service/UserService.java
@@ -1,11 +1,18 @@
 package com.linzx.web.safetyfirst.push.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.google.common.base.Strings;
 import com.linzx.web.safetyfirst.push.bean.api.base.ResponseModel;
 import com.linzx.web.safetyfirst.push.bean.api.user.UpdateInfoModel;
 import com.linzx.web.safetyfirst.push.bean.card.UserCard;
@@ -42,4 +49,125 @@ public class UserService extends BaseService {
 		return ResponseModel.buildOk(card);
 	}
 
+	// 拉取联系人
+    @GET
+    @Path("/contact")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseModel<List<UserCard>> contact() {
+        User self = getSelf();
+        // 拿到我的联系人
+        List<User> users = UserFactory.contacts(self);
+        // 转换为UserCard
+        List<UserCard> userCards = users.stream()
+                // map操作，相当于转置操作，User->UserCard
+                .map(user -> new UserCard(user, true))
+                .collect(Collectors.toList());
+        // 返回
+        return ResponseModel.buildOk(userCards);
+    }
+    
+ // 关注人，
+    // 简化：关注人的操作其实是双方同时关注
+    @PUT // 修改类使用Put
+    @Path("/follow/{followId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseModel<UserCard> follow(@PathParam("followId") String followId) {
+        User self = getSelf();
+
+        // 不能关注我自己
+        if (self.getId().equalsIgnoreCase(followId)
+                || Strings.isNullOrEmpty(followId)) {
+            // 返回参数异常
+            return ResponseModel.buildParameterError();
+        }
+
+
+        // 找到我也关注的人
+        User followUser = UserFactory.findById(followId);
+        if (followUser == null) {
+            // 未找到人
+            return ResponseModel.buildNotFoundUserError(null);
+        }
+
+        // 备注默认没有，后面可以扩展
+        followUser = UserFactory.follow(self, followUser, null);
+        if (followUser == null) {
+            // 关注失败，返回服务器异常
+            return ResponseModel.buildServiceError();
+        }
+
+        // TODO 通知我关注的人我关注他
+
+        // 返回关注的人的信息
+        return ResponseModel.buildOk(new UserCard(followUser, true));
+    }
+    
+ // 获取某人的信息
+    @GET
+    @Path("{id}") // http://127.0.0.1/api/user/{id}
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseModel<UserCard> getUser(@PathParam("id") String id) {
+        if (Strings.isNullOrEmpty(id)) {
+            // 返回参数异常
+            return ResponseModel.buildParameterError();
+        }
+
+
+        User self = getSelf();
+        if (self.getId().equalsIgnoreCase(id)) {
+            // 返回自己，不必查询数据库
+            return ResponseModel.buildOk(new UserCard(self, true));
+        }
+
+
+        User user = UserFactory.findById(id);
+        if (user == null) {
+            // 没找到，返回没找到用户
+            return ResponseModel.buildNotFoundUserError(null);
+        }
+
+
+        // 如果我们之间有关注的记录，则我已关注需要查询信息的用户
+        boolean isFollow = UserFactory.getUserFollow(self, user) != null;
+        return ResponseModel.buildOk(new UserCard(user, isFollow));
+    }
+    
+ // 搜索人的接口实现
+    // 为了简化分页：只返回20条数据
+    @GET // 搜索人，不涉及数据更改，只是查询，则为GET
+    // http://127.0.0.1/api/user/search/
+    @Path("/search/{name:(.*)?}") // 名字为任意字符，可以为空
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseModel<List<UserCard>> search(@DefaultValue("") @PathParam("name") String name) {
+        User self = getSelf();
+
+        // 先查询数据
+        List<User> searchUsers = UserFactory.search(name);
+        // 把查询的人封装为UserCard
+        // 判断这些人是否有我已经关注的人，
+        // 如果有，则返回的关注状态中应该已经设置好状态
+
+        // 拿出我的联系人
+        final List<User> contacts = UserFactory.contacts(self);
+
+        // 把User->UserCard
+        List<UserCard> userCards = searchUsers.stream()
+                .map(user -> {
+                    // 判断这个人是否是我自己，或者是我的联系人中的人
+                    boolean isFollow = user.getId().equalsIgnoreCase(self.getId())
+                            // 进行联系人的任意匹配，匹配其中的Id字段
+                            || contacts.stream().anyMatch(
+                            contactUser -> contactUser.getId()
+                                    .equalsIgnoreCase(user.getId())
+                    );
+
+                    return new UserCard(user, isFollow);
+                }).collect(Collectors.toList());
+        // 返回
+        return ResponseModel.buildOk(userCards);
+    }
 }
